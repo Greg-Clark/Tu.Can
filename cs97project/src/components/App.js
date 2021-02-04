a634cff9a3a3ab4efe4df37e73e58721808930a5
mich-xie <73980497+mich-xie@users.noreply.github.com>
2021-02-03 20:51:23 -0800

    skeleton
    Added code skeleton for chat page

---
+++ cs97project/src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import MessageList from './MessageList';
+import SendMessageForm from './SendMessageForm';
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+           messages: tempData
+        }
+    }
+
+    render() {
+        return (
+        <div>
+            <MessageList messages={this.state.messages}/>
+            <SendMessageForm />
+         </div>
+        );
+    }
+}
+
+const tempData = [
+    {
+      senderId: "Michelle",
+      text: "hello"
+    },
+    {
+      senderId: "Egg",
+      text: "emacs"
+    }
+  ]
---
+++ cs97project/src/components/MessageList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MessageList extends React.Component {
+    render() {
+      return (
+        <ul className="message-list">                 
+            {this.props.messages.map(message => {
+            return (
+                <li key={message.id} className="message">
+                    <div>{message.senderId}</div>
+                    <div>{message.text}</div>
+                </li>
+            )
+            })}
+       </ul>
+       )}
+}
+
+export default MessageList;
