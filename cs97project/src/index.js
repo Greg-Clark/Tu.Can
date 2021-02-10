991f85acb59d16fa7fb67a49ba5ff7b33247c9c9
greg-clark <gregclark03@gmail.com>
2021-02-09 22:45:41 -0800

    Move some of the js rendering components to the css file

--- cs97project/src/index.js
+++ cs97project/src/index.js
@@ -1,6 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import LoginPage from './components/LoginPage'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-ReactDOM.render(<LoginPage />, document.getElementById('root'));
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: null
+    };
+  }
+  myChangeHandler = (event) => {
+    let user = event.target.name;
+    let pass = event.target.value;
+    this.setState({ [user]: pass });
+  };
+
+  submitHandler = (event) => {
+    alert(
+      "a name was submitted: " +
+        this.state.username +
+        " and a password was submitted: " +
+        this.state.password
+    );
+
+    event.preventDefault();
+  };
+
+  render() {
+    
+    return (
+
+      <div className="LoginBox"> 
+      
+      <h1>Yung Milk</h1>
+    
+      <form onSubmit={this.submitHandler}>
+        {/*username*/}
+        <p>username:</p>
+        <input type="text" name="username" onChange={this.myChangeHandler} />
+
+        {/*password*/}
+        <p>password:</p>
+        <input type="text" name="password" onChange={this.myChangeHandler} />
+        <br></br>
+        {/*input*/}
+        <input type="submit" value="Login" />
+
+      </form>
+    </div>
+    );
+  }
+}
+
+//export default LoginPage
+
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
