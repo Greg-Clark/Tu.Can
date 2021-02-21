bcbb61f642874130650fa3e391c50d94e7b17b33
greg-clark <gregclark03@gmail.com>
2021-02-20 23:06:08 -0800

    updated index.js for folder switch

--- cs97project/src/index.js
+++ cs97project/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './Frontend_Files/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
