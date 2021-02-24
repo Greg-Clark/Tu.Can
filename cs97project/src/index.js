3d7a5ed924a1a661e4fddeaaa25832b2a4fe9690
greg-clark <gregclark03@gmail.com>
2021-02-23 21:14:35 -0800

    main go bye bye

--- cs97project/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import LoginPage from './components/LoginPage';
-import AppRouter from './router/AppRouter';
-
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
