387b29e9e62c17817930df20c4b4e9b9e9d23445
karimsaraipour <75566475+karimsaraipour@users.noreply.github.com>
2021-02-26 22:53:50 -0800

    began routing
    -installed react-router
    -introduced loginpage.js
    -about to integrate register page
    
    Co-Authored-By: Greg-Clark <22511466+Greg-Clark@users.noreply.github.com>

--- cs97project/src/components/App.js
+++ cs97project/src/components/App.js
@@ -4,58 +4,74 @@ import Sidebar from './Sidebar';
 import '../styles/App.css';
 import Pusher from 'pusher-js';
 import axios from './axios'
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from "react-router-dom";
+import Loginpage from './Loginpage';
 // import { render } from '@testing-library/react';
 
 
 function App() {
-  const [messages, setMessages] = useState([]);
+	const [messages, setMessages] = useState([]);
 
-  // axios is just another way to handle get, post and so on
-  useEffect (() => {
-    axios.get('/messages/sync')
-      .then(response => {
-        console.log(response.data);
-        setMessages(response.data);
-      })
-  }, []);
+	// axios is just another way to handle get, post and so on
+	useEffect(() => {
+		axios.get('/messages/sync')
+			.then(response => {
+				console.log(response.data);
+				setMessages(response.data);
+			})
+	}, []);
 
-  useEffect(() => {
-    const pusher = new Pusher('8cdc3d1a07077d29caf4', {
-      cluster: 'us3'
-    });
+	useEffect(() => {
+		const pusher = new Pusher('8cdc3d1a07077d29caf4', {
+			cluster: 'us3'
+		});
 
-    const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (newMessage) => {
-      // just alerts the front end when a message is recieved
-      // alert(JSON.stringify(newMessage));
+		const channel = pusher.subscribe('messages');
+		channel.bind('inserted', (newMessage) => {
+			// just alerts the front end when a message is recieved
+			// alert(JSON.stringify(newMessage));
 
-      //append new messages to current message array
-      setMessages([...messages, newMessage]);
-    });
+			//append new messages to current message array
+			setMessages([...messages, newMessage]);
+		});
 
-    // ensures there is only 1 subscriber(listener)
-    return () => {
-      channel.unbind_all();
-      channel.unsubscribe();
-    };
-  }, [messages]); // captures messages since it is a dependency
+		// ensures there is only 1 subscriber(listener)
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]); // captures messages since it is a dependency
 
-  console.log(messages);
-  return (
-    <div className="app">
-      <div className="app_body">
-        <Sidebar />
-        <Chat 
-          messages= {messages}
-          // handleSendMessage={this.handleSendMessage}
-        />
-      </div>
-    </div>
-  );
+	// console.log(messages);
+	return (
+		<Router>
+			<div className="app">
+				<div className="app_body">
+					<Sidebar />
+					<Chat
+						messages={messages}
+					// handleSendMessage={this.handleSendMessage}
+					/>
+
+					<Link to="/loginpage">Sign Out</Link>
+					<Switch>
+						<Route path="/loginpage">
+							<Loginpage />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 // class App extends React.Component {
- 
+
 
 //   state = {
 //     contacts: [],
@@ -65,27 +81,27 @@ function App() {
 
 
 
-  
+
 
 
 
 //   handleSendMessage = (message) => {
-    
+
 //   }; 
 
 //   componentDidMount() {
-    
+
 //   };
 
 //   componentDidUpdate(prevProps, prevState) {
-    
+
 //   };
 
 //   componentWillUnmount() {
 //     console.log('componentWillUnmount');
 //   };
 
-  
+
 //   render() {
 //     return (
 //       <div className="app">
---
+++ cs97project/src/components/Loginpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Loginpage() {
+    return (
+        <h1>HELLO</h1>
+    );
+}
+
+export default Loginpage;
