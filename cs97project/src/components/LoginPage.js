8ac56f0d985d816243d58dcbb542976cf59d8b68
karimsaraipour <75566475+karimsaraipour@users.noreply.github.com>
2021-02-27 00:42:35 -0800

    added firebase auth
    - user can now sign in with google and the app will proceed to the messenger
    
    Co-Authored-By: Greg-Clark <22511466+Greg-Clark@users.noreply.github.com>

--- cs97project/src/components/LoginPage.js
+++ cs97project/src/components/LoginPage.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+// import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,10 +13,28 @@ import {
     useRouteMatch,
     useParams
   } from "react-router-dom";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+//config
+firebase.initializeApp({
+    apiKey: "AIzaSyCwAH05rOuy7BstqOV-owyNwFrp3W8fmfU",
+    authDomain: "cs97project-968d0.firebaseapp.com",
+    projectId: "cs97project-968d0",
+    storageBucket: "cs97project-968d0.appspot.com",
+    messagingSenderId: "932880062417",
+    appId: "1:932880062417:web:dc00eb9252e7dee95f32f0"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
   
 function LoginPage() {
+    const [user] = useAuthState(auth);
     return (
-        <div>         
+        <div>        
         <h1>In progress : Login Page</h1>
         <form>
             {/*username*/}
@@ -22,7 +46,7 @@ function LoginPage() {
             <br></br>
             {/*input*/}
             <Link to="/messaging">
-            <input type="submit" value="Login"/>
+            <SignIn />
             </Link>
             <Link to="/register">
             <input type="submit" value="Register"/>
@@ -31,5 +55,24 @@ function LoginPage() {
         </div>
     );
 }
+
+function SignIn () {
+    //creates google sign in popup window
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+  
+    return (
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+    )
+  }
+  
+  //If there is a current user, this function will sign them out
+  function SignOut () {
+    return auth.currentUser && (
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+  }
 
 export default LoginPage;
