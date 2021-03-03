bd35004160765a95dec94049166f4168f635b03a
19chenterry <78518670+19chenterry@users.noreply.github.com>
2021-03-02 22:35:19 -0800

    Implemented upload file feature with gridfs
    Still displays errors on upload page, but data can be seen on mongodb

--- cs97project/server/routes/file.js
+++ cs97project/server/routes/file.js
@@ -1,81 +1,84 @@
 const path = require('path');
+const util = require("util");
 const express = require('express');
 const multer = require('multer');
-const File = require('../model/file');
+const GridFsStorage = require("multer-gridfs-storage");
 const Router = express.Router();
 
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, './files');
-    },
-    filename(req, file, cb) {
-      cb(null, `${new Date().getTime()}_${file.originalname}`);
-    }
-  }),
-  limits: {
-    fileSize: 1000000 // max file size 1MB = 1000000 bytes
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpeg|jpg|png|pdf|doc|docx|xlsx|xls)$/)) {
-      return cb(
-        new Error(
-          'only upload files with jpg, jpeg, png, pdf, doc, docx, xslx, xls format.'
-        )
-      );
-    }
-    cb(undefined, true); // continue with upload
-  }
+    storage: new GridFsStorage({
+        url: "mongodb+srv://19chenterry:Tc_02052001@cluster0.rdagt.mongodb.net/files",
+        options: { useNewUrlParser: true, useUnifiedTopology: true },
+        file: (req, file) => {
+            const match = ["image/png", "image/jpeg"];
+
+            if (match.indexOf(file.mimetype) === -1) {
+                const filename = `${file.originalname}`;
+                return filename;
+            }
+
+            return {
+                bucketName: "photos",
+                filename: `${file.originalname}`
+            };
+        }
+    })
 });
 
+//var uploadFiles = multer({ storage: storage }).array("multi-files", 10);
+//var upload = multer({ storage: storage }).single("file");
+//var uploadFilesMiddleware = util.promisify(uploadFiles);
+//module.exports = uploadFilesMiddleware;
+
+
 Router.post(
-  '/upload',
-  upload.single('file'),
-  async (req, res) => {
-    try {
-      const { title, description } = req.body;
-      const { path, mimetype } = req.file;
-      const file = new File({
-        title,
-        description,
-        file_path: path,
-        file_mimetype: mimetype
-      });
-      await file.save();
-      res.send('file uploaded successfully.');
-    } catch (error) {
-      res.status(400).send('Error while uploading file. Try again later.');
+    '/upload',
+    upload.single('file'),
+    async (req, res) => {
+        try {
+            const { title, description } = req.body;
+            const { path, mimetype } = req.file;
+            const file = new File({
+                title,
+                description,
+                file_path: path,
+                file_mimetype: mimetype
+            });
+            await GridFsStorage.save();
+            res.send('file uploaded successfully.');
+        } catch (error) {
+            res.status(400).send('Error while uploading file. Try again later.');
+        }
+    },
+    (error, req, res, next) => {
+        if (error) {
+            res.status(500).send(error.message);
+        }
     }
-  },
-  (error, req, res, next) => {
-    if (error) {
-      res.status(500).send(error.message);
-    }
-  }
 );
 
 Router.get('/getAllFiles', async (req, res) => {
-  try {
-    const files = await File.find({});
-    const sortedByCreationDate = files.sort(
-      (a, b) => b.createdAt - a.createdAt
-    );
-    res.send(sortedByCreationDate);
-  } catch (error) {
-    res.status(400).send('Error while getting list of files. Try again later.');
-  }
+    try {
+        const files = await File.find({});
+        const sortedByCreationDate = files.sort(
+            (a, b) => b.createdAt - a.createdAt
+        );
+        res.send(sortedByCreationDate);
+    } catch (error) {
+        res.status(400).send('Error while getting list of files. Try again later.');
+    }
 });
 
 Router.get('/download/:id', async (req, res) => {
-  try {
-    const file = await File.findById(req.params.id);
-    res.set({
-      'Content-Type': file.file_mimetype
-    });
-    res.sendFile(path.join(__dirname, '..', file.file_path));
-  } catch (error) {
-    res.status(400).send('Error while downloading file. Try again later.');
-  }
+    try {
+        const file = await File.findById(req.params.id);
+        res.set({
+            'Content-Type': file.file_mimetype
+        });
+        res.sendFile(path.join(__dirname, '..', file.file_path));
+    } catch (error) {
+        res.status(400).send('Error while downloading file. Try again later.');
+    }
 });
 
 module.exports = Router;
