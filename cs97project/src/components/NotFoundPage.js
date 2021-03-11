d0727ca20c06fb3da316fba917fb58b3602795f9
greg-clark <gregclark03@gmail.com>
2021-03-11 00:18:20 -0800

    Changed link to button on not found page
    Co-Authored-By: karimsaraipour <75566475+karimsaraipour@users.noreply.github.com>
    Co-Authored-By: Roye Fang <63941035+royefang@users.noreply.github.com>

--- cs97project/src/components/NotFoundPage.js
+++ cs97project/src/components/NotFoundPage.js
@@ -14,8 +14,10 @@ function NotFoundPage() {
 				<h1>Sorry this page was not found</h1>
 			</p>
 			<p style={{ textAlign: "center" }}>
-			<br/>
-				<Link to="/">Return to Home </Link>
+				<br/>
+				<button className="buttons" >
+					<Link to="/">Return to Home </Link>
+				</button>
 			</p>
 		</div>
 	);
